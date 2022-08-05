30871f3363eccfb6a164667719e535246e921457
Nakata <ryosuke.nakata@acompany-ac.com>
2022-08-05 13:32:05 +0900

    Add makePieceStr

--- Src/Utils/ParseCsv.ts
+++ Src/Utils/ParseCsv.ts
@@ -32,8 +32,26 @@ export function parseCsv(data: string): [string[], number[][]]{
     return [schema, secrets];
 }
 
+function checkPieceSize(pieceSize: number) {
+    const lowerLimitSize: number = 1
+    const upperLimitSize: number = 1_000_000
+    if (pieceSize < lowerLimitSize) {
+        throw new Error(
+          "piece size " + pieceSize.toString() + " is over " +
+          "specified limit size " + lowerLimitSize.toString()
+        );
+    }
+    if (pieceSize > upperLimitSize) {
+        throw new Error(
+          "piece size " + pieceSize.toString() + " is under " +
+          "specified limit size " + upperLimitSize.toString()
+        );
+    }
+}
+
 // couchbaseに1回で保存できる上限が1MBなので、デフォルトでは1MBで分割する
 export function makePiece(secrets: number[][], pieceSize: number = 1000000): number[][][]{
+    checkPieceSize(pieceSize);
     let bytes: number = 0;
     let secretsPiece: number[][][] = [];
     secretsPiece.push([]);
@@ -47,6 +65,26 @@ export function makePiece(secrets: number[][], pieceSize: number = 1000000): num
     }
     return secretsPiece;
 }
+
+export function makePieceStr(str: string, pieceSize: number = 1000000): string[]{
+    checkPieceSize(pieceSize);
+
+    const strPiece: string[] = [];
+    let current: string = ""
+    for (const ch of str) {
+        current += ch;
+        if (current.length == pieceSize) {
+            strPiece.push(current);
+            current = "";
+        }
+    }
+    if (current.length > 0) {
+        strPiece.push(current);
+    }
+    return strPiece;
+}
+
+
 
 export function parseCsvVector(data: string): number[] {
     const array: string[] = sanitize(data).split(/\r\n|\n|\r/).filter((el)=>el.match(/[^,\s]/)); // サニタイズ処理とcsvの空白分の削除処理
